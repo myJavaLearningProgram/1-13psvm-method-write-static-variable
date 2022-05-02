0f31a50fecd3fdd417fdf3a05496e71ab35bec3e
Ories <tellt1@126.com>
2022-05-02 11:36:29 +0800

    init

---
+++ src/test/java/com/github/hcsp/MainTest.java
@@ -0,0 +1,20 @@
+package com.github.hcsp;
+
+import java.lang.reflect.Field;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void updateStaticVariableTest() throws Exception {
+        Assertions.assertEquals(readStaticVariable().toLowerCase(), "hello");
+        Main.main(null);
+        Assertions.assertEquals(readStaticVariable().toLowerCase(), "hi");
+    }
+
+    private String readStaticVariable() throws Exception {
+        Field field = Main.class.getDeclaredField("message");
+        field.setAccessible(true);
+        return (String) field.get(null);
+    }
+}
